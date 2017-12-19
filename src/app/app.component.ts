f8f066f1d2ff35a99356d0d6bd58a562a1b5cdb8
Moreno, Belen E <belen.e.moreno@accenture.com>
2017-12-19 14:56:13 -0300

    cmt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FileService} from './services/file.service';
+import { subscribeOn } from 'rxjs/operator/subscribeOn';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Image Gallery';
+  errorMessage: string;
+  images:Array<any>=[];
+
+
+constructor (private fileService: FileService){}
+
+ngOnInit(){
+  this.getImageData();
+}
+
+getImageData(){
+  this.fileService.getImages().subscribe(
+    data=> {this.images = data.result},
+    error=> this.errorMessage = error
+  )
+}
+
+refreshImages(status){
+  if(status == true){
+    console.log("Uploaded successfully!");
+    this.getImageData();
+  }
+
+}
 }
